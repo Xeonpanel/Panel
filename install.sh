55d44e36bb1d352ab2fbf1d813ac56346230fd40
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-07-12 18:00:54 +0200

    Cleared up

--- install.sh
+++ install.sh
@@ -5,17 +5,13 @@ read -p "Are you sure you want to continue? [y/n] " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     sudo apt update
-    sudo apt install git
-    apt install python3
-    apt install python3-pip
+    sudo apt install git python3 python3-pip -y
     python3 -m pip install flask
     python3 -m pip install requests
     git clone https://github.com/Xeonpanel/Panel.git
     mkdir -p /var/www/xeonpanel
     mv Panel/* /var/www/xeonpanel
     rm -rf Panel
-    apt install screen
-    screen
     cd /var/www/xeonpanel
     mv /var/www/xeonpanel/xeonpanel.service /etc/systemd/system/
 else
