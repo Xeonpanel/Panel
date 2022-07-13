4d1ef7a4a2e71653b64e6b18e6126fa6e92efb3d
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-07-13 09:25:40 +0200

    Update images.py

--- admin/images.py
+++ admin/images.py
@@ -33,3 +33,23 @@ def create_image():
     else:
         return flask.redirect("/login")
 
+@app.route("/admin/images/<imageid>/view", methods=["GET"])
+def view_image(imageid):
+    if flask.session:
+        if flask.session["user_type"] == "administrator":
+            data = sqlquery("SELECT * FROM images WHERE id = ?", int(imageid),).fetchall()
+            if len(data):
+                return flask.render_template(
+                    "themes/{}/admin/images/viewimage.html".format(app.config["THEME"]),
+                    title="Images",
+                    page="images",
+                    panelname=sqlquery("SELECT panel_name FROM settings").fetchone()[0],
+                    image=sqlquery("SELECT * FROM images WHERE id = ?", int(imageid),).fetchall()[0]
+                )
+            else:
+                flask.abort(404)
+        else:
+            flask.abort(401)
+    else:
+        return flask.redirect("/login")
+
