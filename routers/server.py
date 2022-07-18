a1f366bec670df5909c8bbe7916e593ef2fba4ef
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-07-18 07:45:03 +0200

    Update server.py

--- routers/server.py
+++ routers/server.py
@@ -8,7 +8,21 @@ from waitress import serve
 def server(serverid):
     if flask.session:
         if len(sqlquery("SELECT * FROM servers WHERE owner_id = ? and id = ?", flask.session["id"], serverid)):
-            return flask.render_template("/server/server.html", title="Console", sqlquery=sqlquery, serverid=serverid)
+            data = sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0]
+            imageid = data[6]
+            startup = data[10]
+            if len(sqlquery("SELECT * FROM server_variables WHERE server_id = ?", serverid)):
+                variables = []
+                for variable in sqlquery("SELECT * FROM image_variables WHERE image_id = ?", imageid):
+                    variables.append(variable[2])
+                if any(x in startup for x in variables):
+                    for variable in variables:
+                        variableid = sqlquery("SELECT * FROM image_variables WHERE image_id = ? and variable = ?", imageid, variable)[0][0]
+                        if len(sqlquery("SELECT * FROM server_variables WHERE server_id = ? and image_id = ? and variable_id = ?", serverid, imageid, variableid)):
+                            startup = startup.replace("[[{}]]".format(variable), sqlquery("SELECT * FROM server_variables WHERE server_id = ? and image_id = ? and variable_id = ?", serverid, imageid, variableid)[0][1])
+                        else:
+                            startup = startup.replace("[[{}]]".format(variable), "")
+            return flask.render_template("/server/server.html", title="Console", sqlquery=sqlquery, serverid=serverid, startup=startup)
         else:
             flask.abort(401)
     else:
