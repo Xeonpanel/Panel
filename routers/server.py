0fab0c375cd5a43e5c4331a64eaa762396b7c110
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-07-18 07:32:33 +0200

    Startup command backend

--- routers/server.py
+++ routers/server.py
@@ -2,6 +2,8 @@ import flask, requests, json
 
 from __main__ import app, sqlquery
 
+from waitress import serve
+
 @app.route("/dashboard/server/<serverid>")
 def server(serverid):
     if flask.session:
@@ -26,7 +28,20 @@ def server_files(serverid):
 def server_configuration(serverid):
     if flask.session:
         if len(sqlquery("SELECT * FROM servers WHERE owner_id = ? and id = ?", flask.session["id"], serverid)):
-            startup = "test"
+            data = sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0]
+            imageid = data[6]
+            startup = data[10]
+            if len(sqlquery("SELECT * FROM server_variables WHERE server_id = ?", serverid)):
+                variables = []
+                for variable in sqlquery("SELECT * FROM image_variables WHERE image_id = ?", imageid):
+                    variables.append(variable[2])
+                if any(x in startup for x in variables):
+                    for variable in variables:
+                        variableid = sqlquery("SELECT * FROM image_variables WHERE image_id = ? and variable = ?", imageid, variable)[0][0]
+                        if len(sqlquery("SELECT * FROM server_variables WHERE server_id = ? and image_id = ? and variable_id = ?", serverid, imageid, variableid)):
+                            startup = startup.replace("[[{}]]".format(variable), sqlquery("SELECT * FROM server_variables WHERE server_id = ? and image_id = ? and variable_id = ?", serverid, imageid, variableid)[0][1])
+                        else:
+                            startup = startup.replace("[[{}]]".format(variable), "")
             return flask.render_template("/server/configuration.html", title="Configuration", sqlquery=sqlquery, serverid=serverid, startup=startup)
         else:
             flask.abort(401)
