b018cd742eefe72d1a3ab765f735f17f9f6f764a
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-07-18 11:35:07 +0200

    Create setup.py

---
+++ routers/setup.py
@@ -0,0 +1,51 @@
+import flask, os, time, sys, sqlite3, hashlib
+
+from __main__ import app
+
+@app.route("/setup/register-admin", methods=["POST"])
+def setup_register_admin():
+    conn = sqlite3.connect("database.db", check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.executescript(open("schema.sql").read())
+    conn.commit()
+    cursor.execute("INSERT INTO settings (panel_name) VALUES ('Xeonpanel')")
+    cursor.execute(
+        "INSERT INTO users (name, email, password, token, user_type) VALUES (?, ?, ?, ?, ?)",
+        (
+            flask.request.form.get("username"),
+            flask.request.form.get("email"),
+            hashlib.sha256(
+                flask.request.form.get("password").encode("utf-8")
+            ).hexdigest(),
+            os.urandom(50).hex(),
+            "administrator"
+        )
+    )
+    conn.commit()
+    return flask.redirect("/setup/setup-final")
+
+@app.route("/setup/reboot", methods=["GET"])
+def setup_reboot_server():
+    time.sleep(1)
+    os.execv(sys.executable, ["python"] + sys.argv)
+
+@app.route("/setup/setup-final", methods=["GET"])
+def setup_final():
+    return flask.render_template(
+        "/setup/setupfinal.html",
+        title="Installing"
+    )
+
+@app.route("/setup/getting-started", methods=["GET"])
+def setup_getting_started():
+    return flask.render_template(
+        "/setup/welcome.html",
+        title="Getting Started"
+    )
+
+@app.route("/setup/setup-account", methods=["GET"])
+def setup_account():
+    return flask.render_template(
+        "/setup/setupaccount.html",
+        title="Setup Account"
+    )
