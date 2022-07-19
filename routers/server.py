7a7cf4b871873d6d7fa326219d37546f2697b432
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-07-19 23:11:32 +0200

    Update server.py

--- routers/server.py
+++ routers/server.py
@@ -27,10 +27,27 @@ def server(serverid):
         return flask.redirect("/login")
 
 @app.route("/dashboard/server/<serverid>/files")
-def server_files(serverid):
+@app.route("/dashboard/server/<serverid>/files/<path:dir>")
+def server_files(serverid, **dir):
     if flask.session:
         if len(sqlquery("SELECT * FROM servers WHERE owner_id = ? and id = ?", flask.session["id"], serverid)):
-            return flask.render_template("/server/files.html", title="File Manager", sqlquery=sqlquery, serverid=serverid, requests=requests, json=json)
+            if dir:
+                payload = {
+                    "user_token": flask.session["token"],
+                    "path": dir["dir"]
+                }
+                subpath = dir["dir"]
+                path = "/home/container/{}".format(dir["dir"])
+                files = requests.get("https://{}:8080/api/servers/{}/files".format(sqlquery("SELECT * FROM nodes WHERE id = ?", sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0][5])[0][4], sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0][9]), data=payload).json()
+            else:
+                payload = {
+                    "user_token": flask.session["token"],
+                    "path": "/"
+                }
+                subpath = "/"
+                path = "/home/container"
+                files = requests.get("https://{}:8080/api/servers/{}/files".format(sqlquery("SELECT * FROM nodes WHERE id = ?", sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0][5])[0][4], sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0][9]), data=payload).json()
+            return flask.render_template("/server/files.html", title="File Manager", sqlquery=sqlquery, serverid=serverid, json=json, files=files, path=path, subpath=subpath)
         else:
             flask.abort(401)
     else:
@@ -55,6 +72,21 @@ def server_configuration(serverid):
                         else:
                             startup = startup.replace("[[{}]]".format(variable), "")
             return flask.render_template("/server/configuration.html", title="Configuration", sqlquery=sqlquery, serverid=serverid, startup=startup)
+        else:
+            flask.abort(401)
+    else:
+        return flask.redirect("/login")
+
+@app.route("/dashboard/server/<serverid>/files/edit/<path:dir>")
+def edit_file(serverid, **dir):
+    if flask.session:
+        if len(sqlquery("SELECT * FROM servers WHERE owner_id = ? and id = ?", flask.session["id"], serverid)):
+            payload = {
+                "user_token": flask.session["token"],
+                "file": dir["dir"]
+            }
+            file = requests.get("https://{}:8080/api/servers/{}/files/edit".format(sqlquery("SELECT * FROM nodes WHERE id = ?", sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0][5])[0][4], sqlquery("SELECT * FROM servers WHERE id = ?", serverid)[0][9]), data=payload).text
+            return flask.render_template("/server/editfile.html", title="Edit File", sqlquery=sqlquery, content=file, serverid=serverid, path=dir["dir"])
         else:
             flask.abort(401)
     else:
