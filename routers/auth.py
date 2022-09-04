04b1ea6b487bb401c939e5c43588130beff33efe
Snikkertjuh <snikker.zakelijk@protonmail.com>
2022-09-04 12:02:28 +0200

    Update

--- routers/auth.py
+++ routers/auth.py
@@ -26,6 +26,14 @@ def postlogin():
     else:
         return flask.jsonify({"status": "error", "message": "Please fill in all fields"})
 
+@app.get("/register")
+def getregister():
+    return flask.render_template("/auth/register.html", title="Register", sqlquery=query)
+
+@app.post("/register")
+def postregister():
+    return flask.jsonify({"status": "error", "message": "Try again later.."})
+
 # @app.route("/register", methods=["GET", "POST"])
 # def auth_register():
 #     if flask.request.method == "GET":
