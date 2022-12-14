aa456b8cb368b1053e12e4bfa71523527ff48b6d
Jeff <99515656+Jeff53978@users.noreply.github.com>
2022-12-14 04:23:50 +0100

    Update setup.py

--- routers/setup.py
+++ routers/setup.py
@@ -1,10 +1,26 @@
-import flask, os, time, sys, hashlib
-
-from __main__ import app
+import flask, os, time, datetime
+from __main__ import db, bycrypt, app
+from models import Users
 
 @app.post("/setup/register-admin")
 def setup_register_admin():
-    pass
+    username = flask.request.form["username"]
+    password = flask.request.form["password"]
+    email = flask.request.form["email"]
+
+    if username and password and email:
+        if not os.path.exists("database/database.sqlite"):
+            with app.app_context():
+                db.create_all()
+                db.session.commit()
+        with app.app_context():
+            hashed_password = bycrypt.generate_password_hash(password).decode("utf-8")
+            admin = Users(username=username, password=hashed_password, email=email, admin=True, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+            db.session.add(admin)
+            db.session.commit()
+        
+        os.remove("routers/setup.py")
+        return flask.redirect("/setup/setup-final")
 
 @app.get("/setup/finish")
 def setup_reboot_server():
