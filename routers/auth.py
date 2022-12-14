84db745d1f12aa889f5496c14608ce143563fb14
Jeff <99515656+Jeff53978@users.noreply.github.com>
2022-12-14 06:28:26 +0100

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,4 @@
-import flask, os
+import flask, os, datetime
 from __main__ import app, db, bcrypt
 from models import Users
 
@@ -26,18 +26,21 @@ def postlogin():
 
 @app.post("/register")
 def postregister():
-    username = flask.request.form.get("username")
-    password = flask.request.form.get("password")
-    email = flask.request.form.get("email")
+    username = flask.request.json.get("username")
+    password = flask.request.json.get("password")
+    email = flask.request.json.get("email")
     if username and password and email:
         if Users.query.filter_by(username=username).first():
             return flask.jsonify({"status": "error", "message": "Username already exists"})
         elif Users.query.filter_by(email=email).first():
             return flask.jsonify({"status": "error", "message": "Email already exists"})
         else:
-            user = Users(username=username, password=bcrypt.generate_password_hash(password), email=email, admin=False, created_at=flask.datetime.datetime.now(), updated_at=flask.datetime.datetime.now())
+            user = Users(username=username, password=bcrypt.generate_password_hash(password), email=email, admin=False, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
             db.session.add(user)
             db.session.commit()
+            flask.session["user_id"] = user.id
+            flask.session["admin"] = user.admin
+            flask.session["csrf_token"] = os.urandom(50).hex()
             return flask.jsonify({"status": "success", "message": "Account created"})
     else:
         return flask.jsonify({"status": "error", "message": "Please fill in all fields"})
