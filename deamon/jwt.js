6647581e80d099016ab5df0caafd4950510c08dd
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 22:03:15 +0200

    Update jwt.js

--- deamon/jwt.js
+++ deamon/jwt.js
@@ -1,2 +1,30 @@
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+const { Token } = require("./models");
+
+async function requireJWT(req, res, next) {
+    const jwtToken = req.headers["Authorization"];
+
+    if (!jwtToken) {
+        return res.json({ success: false, error: "Unauthorized" });
+    }
+
+    try {
+        const token = await Token.findAll();
+
+        if (token.length <= 0) {
+            return res.json({ success: false, error: "Internal Server Error" });
+        }
+
+        const jwtKey = token[0].jwt_key;
+        const decoded = jwt.verify(jwtToken, jwtKey);
+        if (!decoded.authorized) {
+            return res.json({ success: false, error: "Unauthorized" });
+        }
+        next();
+    } catch (err) {
+        return res.json({ success: false, error: "Unauthorized" });
+    }
+};
+
+module.exports = { requireJWT };
