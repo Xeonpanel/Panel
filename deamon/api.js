3f7f8adda52f4419b5a7073c106e99b5496c5533
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 21:11:38 +0200

    Update api.js

--- deamon/api.js
+++ deamon/api.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const crypto = require("crypto");
 const { Token } = require("./models");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const router = express.Router();
 
 router.get("/jwt", async (req, res) => {
     const randomBytes = crypto.randomBytes(24).toString("hex");
+    req.session.randomBytes = randomBytes;
     const publicKey = await Token.findOne().public_key;
 
     const encryptedBytes = crypto.publicEncrypt(publicKey, Buffer.from(randomBytes));
     const encryptedText = encryptedBytes.toString("base64");
 
     return res.send(encryptedText);
+});
+
+router.post("/jwt", async (req, res) => {
+    const { randomBytes } = req.session;
+    const { decryptedText } = req.body;
+
+    if (!randomBytes || !decryptedText) {
+        return res.status(400).send("Bad Request");
+    }
+
+    if (randomBytes !== decryptedText) {
+        return res.status(401).send("Unauthorized");
+    }
+
+    const jwtToken = jwt.sign({ authorized: true }, fs.readFileSync("./ssl/key.pem"), { algorithm: "RS256" });
+    res.send(jwtToken);
 });
 
 module.exports = router;
