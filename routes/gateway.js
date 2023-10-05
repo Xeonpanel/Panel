3484a4369e7909855bb4f967abfe10cf195e4130
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 21:51:24 +0200

    Update gateway.js

--- routes/gateway.js
+++ routes/gateway.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { User, Node, Port } = require("../models");
 const crypto = require("crypto");
+const process = require("process");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 async function generateJWT(node) {
     let resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
