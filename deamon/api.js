ad9ddae20f22b16fdd2c40a748af4e55e2761ceb
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 21:01:58 +0200

    Update api.js

--- deamon/api.js
+++ deamon/api.js
@@ -5,7 +5,13 @@ const { Token } = require("./models");
 const router = express.Router();
 
 router.get("/jwt", async (req, res) => {
-    
+    const randomBytes = crypto.randomBytes(24).toString("hex");
+    const publicKey = await Token.findOne().public_key;
+
+    const encryptedBytes = crypto.publicEncrypt(publicKey, Buffer.from(randomBytes));
+    const encryptedText = encryptedBytes.toString("base64");
+
+    return res.send(encryptedText);
 });
 
 module.exports = router;
