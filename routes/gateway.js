debe028064983864277970105396710c061037cf
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 20:51:15 +0200

    Update gateway.js

--- routes/gateway.js
+++ routes/gateway.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { User, Node, Port } = require("../models");
+const crypto = require("crypto");
+
+async function generateJWT(node) {
+    let resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
+        method: "GET",
+    });
+
+    let encryptedBytes = await resp.text();
+    let decryptedBytes = crypto.privateDecrypt(node.private_key, Buffer.from(encryptedBytes, "base64"));
+    let decryptedText = decryptedBytes.toString("utf8");
+
+    resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
+        method: "POST",
+        body: decryptedText,
+    });
+
+    return await resp.text();
+}
 
 module.exports = router;
