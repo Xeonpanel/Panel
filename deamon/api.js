b56e968b6a20ec8c03e2d179f222b14462904c7c
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 21:00:50 +0200

    Create api.js

---
+++ deamon/api.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const crypto = require("crypto");
+const { Token } = require("./models");
+
+const router = express.Router();
+
+router.get("/jwt", async (req, res) => {
+    
+});
+
+module.exports = router;
