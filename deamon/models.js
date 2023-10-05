4144e4cd21b86fa2bd061dc57afa7b27cff9d389
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 21:00:57 +0200

    Create models.js

---
+++ deamon/models.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db.sqlite3",
+    logging: false
+});
+
+const Token = sequelize.define("token", {
+    public_key: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+Token.sync();
+
+module.exports = Token;
