8c48eaee71b66d721c91f57d9c08f2a8f126d6bc
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 21:00:52 +0200

    Create index.js

---
+++ deamon/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const parser = require("body-parser");
+const session = require("express-session");
+const selfsigned = require("selfsigned");
+const flash = require("connect-flash");
+const crypto = require("crypto");
+const chalk = require("chalk");
+const https = require("https");
+const path = require("path");
+const os = require("os")
+const fs = require("fs");
+
+if (!fs.existsSync("./ssl/key.pem") || !fs.existsSync("./ssl/cert.pem")) {
+    console.log(`[${chalk.red("Error")}] SSL certificate not found`);
+    console.log(`[${chalk.yellow("Warn")}] Generating SSL certificate`);
+
+    const attrs = [{ name: "commonName", value: os.hostname() }];
+    const pems = selfsigned.generate(attrs, { days: 90 });
+
+    try {
+    fs.mkdirSync(path.join(__dirname, "ssl"));
+    } catch (err) {}
+
+    fs.writeFileSync(path.join(__dirname, "ssl", "key.pem"), pems.private);
+    fs.writeFileSync(path.join(__dirname, "ssl", "cert.pem"), pems.cert);
+    console.log(`[${chalk.green("Success")}] SSL certificate generated`);
+}
+
+const app = express();
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+
+app.use("/api", require("./api"));      
+
+https.createServer({
+    key: fs.readFileSync("./ssl/key.pem"),
+    cert: fs.readFileSync("./ssl/cert.pem"),
+}, app).listen(3001, () => {
+    console.log(`[${chalk.cyan("Info")}] Using SSL certificate from ${path.join(__dirname, "ssl", "cert.pem")}`);
+    console.log(`[${chalk.green("Success")}] Deamon Server started on https://localhost:3001`);
+});
