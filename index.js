de40b789e2f818b75bc353f2e8739af32a935e5d
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 19:30:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const checkAuth = require("./middleware/checkAuth");
+const express = require("express");
+const parser = require("body-parser");
+const session = require("express-session");
+const selfsigned = require("selfsigned");
+const flash = require("connect-flash");
+const crypto = require("crypto");
+const chalk = require("chalk");
+const https = require("https");
+const path = require("path");
+const os = require("os")
+const fs = require("fs");
+
+
+if (!fs.existsSync("./ssl/key.pem") || !fs.existsSync("./ssl/cert.pem")) {
+    console.log(`[${chalk.red("Error")}] SSL certificate not found`);
+    console.log(`[${chalk.yellow("Warn")}] Generating SSL certificate`);
+
+    const attrs = [{ name: "commonName", value: os.hostname() }];
+    const pems = selfsigned.generate(attrs, { days: 90 });
+
+    try {
+    fs.mkdirSync(path.join(__dirname, "ssl"));
+    } catch (err) {}
+
+    fs.writeFileSync(path.join(__dirname, "ssl", "key.pem"), pems.private);
+    fs.writeFileSync(path.join(__dirname, "ssl", "cert.pem"), pems.cert);
+    console.log(`[${chalk.green("Success")}] SSL certificate generated`);
+}
+
+const app = express();
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(session({
+    // secret: crypto.randomBytes(24).toString("hex"),
+    secret: "test",
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(flash());
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/", checkAuth, async (req, res) => {
+    return res.redirect("/dashboard");
+});
+
+app.use("/auth", require("./routes/auth"));      
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/admin", require("./routes/admin"));
+
+https.createServer({
+    key: fs.readFileSync("./ssl/key.pem"),
+    cert: fs.readFileSync("./ssl/cert.pem"),
+}, app).listen(3000, () => {
+    console.log(`[${chalk.cyan("Info")}] Using SSL certificate from ${path.join(__dirname, "ssl", "cert.pem")}`);
+    console.log(`[${chalk.green("Success")}] Server started on https://localhost:3000`);
+});
