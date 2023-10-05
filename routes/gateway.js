b484ea72e9fe075ad14ec6a4ddd6bfbf80e37b80
jeff9014223 <143845476+jeff9014223@users.noreply.github.com>
2023-10-05 22:43:30 +0200

    Update gateway.js

--- routes/gateway.js
+++ routes/gateway.js
@@ -3,29 +3,35 @@ const router = express.Router();
 const { User, Node, Port } = require("../models");
 const crypto = require("crypto");
 const process = require("process");
+const chalk = require("chalk");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 async function generateJWT(node) {
-    let resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
-        method: "GET",
-    });
-
-    let cookies = resp.headers.get("set-cookie");
-    let encryptedBytes = await resp.text();
-    let decryptedBytes = crypto.privateDecrypt(node.private_key, Buffer.from(encryptedBytes, "base64"));
-    let decryptedText = decryptedBytes.toString();
-
-    resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
-        method: "POST",
-        body: JSON.stringify({ decryptedBytes: decryptedText }),
-        headers: {
-            "Cookie": cookies,
-            "Content-Type": "application/json"
-        }
-    });
-
-    return await resp.text();
+    try {
+        let resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
+            method: "GET",
+        });
+    
+        let cookies = resp.headers.get("set-cookie");
+        let encryptedBytes = await resp.text();
+        let decryptedBytes = crypto.privateDecrypt(node.private_key, Buffer.from(encryptedBytes, "base64"));
+        let decryptedText = decryptedBytes.toString();
+    
+        resp = await fetch(`https://${node.ip}:${node.port}/api/jwt`, {
+            method: "POST",
+            body: JSON.stringify({ decryptedBytes: decryptedText }),
+            headers: {
+                "Cookie": cookies,
+                "Content-Type": "application/json"
+            }
+        });
+    
+        return await resp.text();
+    } catch (err) {
+        console.log(`[${chalk.red("Error")}] Node ${node.id} is offline`);
+        return null;
+    }
 }
 
 const jwtCache = {};
